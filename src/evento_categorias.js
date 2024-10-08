178583ea2d91624d26c4102c7785e55cc91f15c0
dagarciagil <diegoalexandergarciagil@gmail.com>
2024-10-08 14:54:44 -0500

    Prueba 1
    Prueba1

--- src/evento_categorias.js
+++ src/evento_categorias.js
@@ -8,24 +8,24 @@ let categoriaActiva;
 // Agregamos el event listener a las categorias.
 contenedorCategorias.addEventListener('click', (e) => {
     e.preventDefault();
-    
+
     // Comprobamos que lo que se le hace click contenga el data-categoria.
     if (e.target.closest('a')) {
         categoriaActiva = e.target.closest('a').dataset.categoria;
         galeria.dataset.categoria = categoriaActiva;
 
         // Obtenemos las fotos que corresponden a la categoria
-        const docentes = dataDocentes.docentes[categoriaActiva]; 
-      
+        const docentes = dataDocentes.docentes[categoriaActiva];
+
         // Agregamos un overflow hidden al body.
         document.body.style.overflow = 'hidden';
         // Agregamos la clase active a la galeria.
         galeria.classList.add('galeria--active');
-       
+
         // Tomamos la informacion de la primera imagen de la categoria y la cargamos en el DOM.
         const { nombre, ruta, nombre_citaciones } = docentes[0];
         cargarDocentes(nombre, ruta, nombre_citaciones);
-      
+
     }
 });
 
